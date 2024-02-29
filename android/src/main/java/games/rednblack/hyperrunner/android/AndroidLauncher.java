e34b1d903c166a87f7f530c4bce8f302562f3584
Ed <eduardas.lapinskas@gmail.com>
2024-02-29 14:54:03 +0200

    first amend
    Basic project setup

---
+++ android/src/main/java/games/rednblack/hyperrunner/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package games.rednblack.hyperrunner.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import games.rednblack.hyperrunner.HyperRunner;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new HyperRunner(), configuration);
+    }
+}
