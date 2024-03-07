2ad78e802fa838f3a5573e1352e954447ca7f90f
Tadas Gataveckas <115645380+tadasgataveckas@users.noreply.github.com>
2024-03-07 23:13:50 +0200

    Prideta navigacija, pradzioje rodomas HomeFragment.java

--- app/src/main/java/com/example/audiobook_app/MainActivity.java
+++ app/src/main/java/com/example/audiobook_app/MainActivity.java
@@ -9,6 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 
 import androidx.core.view.WindowCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -27,21 +30,22 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        replaceFragment( new HomeFragment());
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            if(item.getItemId() == R.id.homeNavMenu) {
+                replaceFragment(new HomeFragment());
             }
+            else if (item.getItemId() == R.id.profileNavMenu) {
+                replaceFragment(new ProfileFragment());
+            }
+            else if (item.getItemId() == R.id.settingsNavMenu){
+                replaceFragment(new SettingsFragment());
+            }
+            return true;
         });
+
+
     }
 
     @Override
@@ -49,5 +53,13 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayoutNavigation,fragment);
+        fragmentTransaction.commit();
     }
 }
