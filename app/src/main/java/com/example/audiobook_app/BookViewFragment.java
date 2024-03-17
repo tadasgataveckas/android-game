bc49b914a6a9adbf6910843ae59b0a8fab3d52a4
Tadas Gataveckas <115645380+tadasgataveckas@users.noreply.github.com>
2024-03-17 23:34:06 +0200

    Ikeltas pataisytas Roko Kaulecko darbas
    Pervadintos klases uzkomentuotas "music samples" is assetu

--- app/src/main/java/com/example/audiobook_app/BookViewFragment.java
+++ app/src/main/java/com/example/audiobook_app/BookViewFragment.java
@@ -4,36 +4,37 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.audiobook_app.databinding.FragmentSecondBinding;
+import com.example.audiobook_app.databinding.FragmentBookViewBinding;
+//Roko Kaulecko
+public class BookViewFragment extends Fragment {
 
-public class SecondFragment extends Fragment {
-
-    private FragmentSecondBinding binding;
+    private FragmentBookViewBinding binding;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        // Inflate the layout for this fragment using view binding
+        binding = FragmentBookViewBinding.inflate(inflater, container, false);
         return binding.getRoot();
-
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                NavHostFragment.findNavController(BookViewFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
     }
