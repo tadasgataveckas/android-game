b038d02e6c902afb6cc61e333830aee89892cf90
RokasKau <161127432+RokasKau@users.noreply.github.com>
2024-05-24 12:46:01 +0300

    animacija

---
+++ app/src/main/java/com/example/audiobook_app/Activity/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.audiobook_app.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.audiobook_app.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+    private Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        handler.postDelayed(runnable, 8000);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
+    }
+}
